e32d3b4ea86a9cffe6818712de0ffb378950f165
IPivkin <IPivkin@sportmasterlab.net>
2023-02-14 15:24:11 +0300

    m1-init Review fix.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm")
 }
 
 group = "ru.plantkeeper"
@@ -20,5 +20,5 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "17"
 }
